963543bdcb2e5f480d1a91460da9668ed94f2dd2
subhani <babasubhani2004@gmail.com>
2023-09-26 09:56:58 +0000

    solution

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+app.get("/players/", async (request, response) => {
+  const playersQuery = `
+    SELECT
+      *
+    FROM 
+      cricket_team;   
+    `;
+
+  let playersList = await db.all(playersQuery);
+  response.send(playersList);
+});
